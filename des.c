b3fa20df74541e5bb46980b5829ae457ca3638c1
Matt Jag <jagodzinskimatt@gmail.com>
2017-10-22 00:46:14 -0400

    Completed initial key permutation.\nvisualIKP:  Allows for verbose messages of the Initial Key Permutation to appear

--- des.c
+++ des.c
@@ -6,15 +6,17 @@
 #define LEVEL_ONE 1
 #define LEVEL_TWO 2
 
-int verbose = LEVEL_TWO;
+int verbose = LEVEL_ONE;
+bool visualIKP = false;
 
 unsigned char key[8] = { 255, 255, 255, 255, 255, 255, 255, 255};
 
 // return pc1 applied to the key
 void initialKeyPermutation(unsigned char pk[])
 {
-	if (verbose >= LEVEL_ONE)
+	if (verbose >= LEVEL_ONE && visualIKP)
 	{
+		printf(" ---- Initial Key Permutationa ---- \n");
 		printf(" - Key - \n");
 		printf("%s %s %s %s %s %s %s %s\n", byteToBinaryString(key[0]), byteToBinaryString(key[1]),
 										byteToBinaryString(key[2]), byteToBinaryString(key[3]),
@@ -29,55 +31,77 @@ void initialKeyPermutation(unsigned char pk[])
 	}
 	unsigned char weight;
 	unsigned char permutatedVal;
-	for (int byte = 0; byte < sizeof(key); byte++)
+	for (int byte = 0; byte < sizeof(key); byte++) // limit to first byte for testing
 	{
 		weight = 1;
 		for (int bit = 6; bit >= 0; bit--)
 		{
 			permutatedVal = pc1[byte][bit] - 1;
-			if (verbose >= LEVEL_TWO)
+			if (verbose >= LEVEL_TWO  && visualIKP)
 			{
 				printf("----------\n");
 				printf("PC-1 : %d\n", permutatedVal + 1);
 				printf(" - Key - \n");
 				printf("       8        16       24       32       40       48       56       64\n");
 				printf("%s %s %s %s %s %s %s %s\n", byteToBinaryString(key[0]), byteToBinaryString(key[1]),
-										byteToBinaryString(key[2]), byteToBinaryString(key[3]),
-										byteToBinaryString(key[4]), byteToBinaryString(key[5]),
-										byteToBinaryString(key[6]), byteToBinaryString(key[7]));
+													byteToBinaryString(key[2]), byteToBinaryString(key[3]),
+													byteToBinaryString(key[4]), byteToBinaryString(key[5]),
+													byteToBinaryString(key[6]), byteToBinaryString(key[7]));
+				//Key Pointer
 				for (int i = 0; i < permutatedVal; i++)
 				{
 					i%8==7? printf("  "): printf(" ");
 				}
 				printf("|\n");
-				int lowValue = permutatedVal < byte*8 + bit? permutatedVal: byte*8 + bit; //implementation of min
-				int highValue = permutatedVal > byte*8 + bit? permutatedVal: byte*8 + bit; //implementation of max
+				
+				//printf("Key Position: %d\n", permutatedVal);
+				//printf("Permutated Position: %d\n", byte*8 + bit + 1);
+
+				int lowValue = permutatedVal < byte*8 + bit + 1? permutatedVal: byte*8 + bit + 1; //implementation of min
+				int highValue = permutatedVal > byte*8 + bit + 1? permutatedVal: byte*8 + bit + 1; //implementation of max
+
 				for (int i = 0; i < lowValue; i++)
 				{
-					i%8==7? printf("  "): printf(" ");
+					printf(" ");
+					if (i%8==7)
+						printf(" ");
 				}
-				printf("+");
-				for (int i = 1; i < highValue - lowValue; i++)
+				
+				bool extra_space;
+				if (highValue != lowValue)
 				{
-					i%8==7 ? printf("--"): printf("-");
+					printf("+");
+					extra_space = lowValue%8==7;
+					for (int i = lowValue + 1; i < highValue; i++)
+					{
+						if (extra_space && i%8==0)
+							printf("-");
+						printf("-");
+						extra_space = i%8==7;
+					}
+					if (highValue%8==0)
+						printf("-");
 				}
 				printf("+\n");
-				for (int i = 0; i < byte*8 + bit; i++)
+				
+				extra_space = false;
+				for (int i = 0; i < byte*8 + bit + 1; i++)
 				{
-					i%8==7 ? printf("  "): printf(" ");
+					if (extra_space && i%8==0)
+						printf(" ");
+					printf(" ");
+					extra_space = i%8==7;
 				}
+				if ((bit + 1)%8==0)
+					printf(" ");
 				printf("|\n");
-				printf("%s %s %s %s %s %s %s %s\n", byteToBinaryString(pk[0]), byteToBinaryString(pk[1]),
-										byteToBinaryString(pk[2]), byteToBinaryString(pk[3]),
-										byteToBinaryString(pk[4]), byteToBinaryString(pk[5]),
-										byteToBinaryString(pk[6]), byteToBinaryString(pk[7]));
 				//For Debugging: printf("PK = %lu\n", pk[byte]);
 				//For Debugging: printf("key[%d][%d] : %d\n", permutatedVal/8, permutatedVal%8, getbit(key[(permutatedVal)/8], permutatedVal%8));
 				//For Debugging: printf("weight: %d\n", weight);
 			}
 			pk[byte] += getbit(key[(permutatedVal)/8], permutatedVal%8) * weight;
 			weight *= 2;
-			if (verbose >= LEVEL_TWO)
+			if (verbose >= LEVEL_TWO  && visualIKP)
 			{
 				printf("%s %s %s %s %s %s %s %s\n", byteToBinaryString(pk[0]), byteToBinaryString(pk[1]),
 										byteToBinaryString(pk[2]), byteToBinaryString(pk[3]),
@@ -85,27 +109,40 @@ void initialKeyPermutation(unsigned char pk[])
 										byteToBinaryString(pk[6]), byteToBinaryString(pk[7]));
 			}
 		}
-		if (verbose == LEVEL_ONE)
+		if (verbose == LEVEL_ONE  && visualIKP)
 		{
 			printf("%s %s %s %s %s %s %s %s\n", byteToBinaryString(pk[0]), byteToBinaryString(pk[1]),
 												byteToBinaryString(pk[2]), byteToBinaryString(pk[3]),
 												byteToBinaryString(pk[4]), byteToBinaryString(pk[5]),
 												byteToBinaryString(pk[6]), byteToBinaryString(pk[7]));
 		}
+		else if (verbose == LEVEL_TWO  && visualIKP)
+		{
+			printf("---------\n");
+		}
 	}
-	if (verbose >= LEVEL_ONE)
+	if (verbose >= LEVEL_ONE  && visualIKP)
 	{
 		printf("- Permuted Key - \n");
 		printf("%s %s %s %s %s %s %s %s\n", byteToBinaryString(pk[0]), byteToBinaryString(pk[1]),
 										byteToBinaryString(pk[2]), byteToBinaryString(pk[3]),
 										byteToBinaryString(pk[4]), byteToBinaryString(pk[5]),
 										byteToBinaryString(pk[6]), byteToBinaryString(pk[7]));
+		printf(" ---------------------------------- \n");
 	}
 }
+
+void keyRounds(unsigned char keys[][])
+{
+
+}
+
+
 
 int main(int argc, char *argv[])
 {
 	printf("DES: Code in Development\n");
 	unsigned char pk[8] = {0,0,0,0,0,0,0,0};
 	initialKeyPermutation(pk);
+	unsigned char keys[16][];//[number of key rounds][size of key in bytes]
 }
